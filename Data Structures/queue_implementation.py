6a6efab1872231ac219e56edf4f0881436113293
adamhu714 <adamhu714@gmail.com>
2024-01-04 05:20:13 +0000

    Completed queue with linked list

--- Data Structures/queue_implementation.py
+++ Data Structures/queue_implementation.py
@@ -13,10 +13,45 @@ from queue import Queue
 # But ourselves, we'd use linked list
 
 class Linked_List:
-    pass
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        node = self.head
+        while node != None:
+            yield node
+            node = node.next
+    
+    def __repr__(self) -> str:
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " <- ".join(nodes)
+
+    def remove_from_head(self):
+        if self.head == None:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        return temp.val
+    
+    def add_to_tail(self, node):
+        if self.head == None:
+            self.head = node
+            self.tail = node
+            return
+        # I think this works if the nodes are passed by reference. Pointers in c++ made more sense to me.
+        self.tail.next = node
+        self.tail = node
 
 class Node:
-
     def __init__(self, value) -> None:
-        self.value = value
+        self.val = value
         self.next = None
+
+    def add_next(self, node):
+        self.next = node
+
+    def __repr__(self) -> str:
+        return self.val
