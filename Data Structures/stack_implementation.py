cbaf07e80f03cec1f6412e09299910853414e63a
adamhu714 <adamhu714@gmail.com>
2024-01-04 04:43:38 +0000

    Finished stack implementation

--- Data Structures/stack_implementation.py
+++ Data Structures/stack_implementation.py
@@ -1,6 +1,23 @@
 # Stack LIFO list, easy implementation with list in python
 # O(1) push, pop, peak, length
-# O(n) to access any location
 
 class Stack:
-    pass
+    
+    def __init__(self) -> None:
+        self.values = []
+
+    def push(self, value) -> None:
+        self.values.append(value)
+
+    def pop(self):
+        if len(self.values) == 0:
+            return None
+        return self.values.pop()
+    
+    def peek(self):
+        if len(self.values) == 0:
+            return None
+        return self.values[-1]
+    
+    def size(self):
+        return len(self.values)
