6862cd610009f668a41c3579db3d1e0e65cb5aef
adamhu714 <adamhu714@gmail.com>
2024-01-23 21:30:06 +0000

    Started Go Loops

---
+++ 12-Learn-Go/7-Loops/1-For/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bulkSend(numMessages int) float64 {
+	cost := 1.00
+	for i := 0; i < numMessages; i++ {
+		cost += 0.01
+	}
+	return cost
+}
+
+// don't edit below this line
+
+func test(numMessages int) {
+	fmt.Printf("Sending %v messages\n", numMessages)
+	cost := bulkSend(numMessages)
+	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+	fmt.Println("===============================================================")
+}
+
+func main() {
+	test(10)
+	test(20)
+	test(30)
+	test(40)
+	test(50)
+}
